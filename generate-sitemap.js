3caefa02cd71aae2dc2a72b55396e10ce876b1a3
sharyark <ksharyar12345@gmail.com>
2024-06-03 08:54:52 +0000

    updated

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 async function generateSitemap() {
-  const sitemap = new SitemapStream({ hostname: 'https://yourdomain.com' });
+  const sitemap = new SitemapStream({ hostname: 'https://www.shinkawelfare.online/' });
 
   const writeStream = createWriteStream(path.join(__dirname, 'public', 'sitemap.xml'));
   sitemap.pipe(writeStream);
