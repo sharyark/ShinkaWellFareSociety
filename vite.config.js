7cd1209cbf3f26ebfaadda34ad34b3fb12f6e6f8
sharyar khan <ksharyar12345@gmail.com>
2025-09-18 21:51:54 +0500

    site map

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteSitemapPlugin } from "vite-plugin-sitemap";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
 
+
+export default defineConfig({
+  plugins: [
+    react(),
+    ViteSitemapPlugin({
+      hostname: "https://www.shinkawelfaresociety.com", // apni domain
+      urls: [
+        "/",
+        "/home",
+        "/about",
+        "/service",
+        "/contact",
+        "/donate",
+        "/abulance",   // spelling correct
+        "/cleanliness",
+        "/water",
+        "/food",
+        "/educational",
+        "/burial",
+        "/team",
+        "/financial",
+      ],
+      changefreq: "daily",
+      priority: 0.7,
+    }),
+  ],
+});
